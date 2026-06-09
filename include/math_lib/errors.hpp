5b8a9dde0b2bc8ba0ed6fcdd4d528a07670279d7
Petr <lab.chern@mail.ru>
2026-06-09 21:50:48 +0000

    fix: made method public

--- include/math_lib/errors.hpp
+++ include/math_lib/errors.hpp
@@ -1,37 +1,43 @@
 #pragma once
 #include <exception>
 
-class CalculationError : std::exception {
+class CalculationError : public std::exception {
+public:
     const char *what() const noexcept override {
         return "Unknown error";
     }
 };
 
-class DivisionByZero : CalculationError {
+class DivisionByZero : public CalculationError {
+public:
     const char *what() const noexcept override {
         return "Cannot divide bu zero";
     }
 };
 
-class Overflow : CalculationError {
+class Overflow : public CalculationError {
+public:
     const char *what() const noexcept override {
         return "Integer overflow";
     }
 };
 
-class Underflow : CalculationError {
+class Underflow : public CalculationError {
+public:
     const char *what() const noexcept override {
         return "Integer underflow";
     }
 };
 
-class NegativePower : CalculationError {
+class NegativePower : public CalculationError {
+public:
     const char *what() const noexcept override {
         return "Didn't implement taking roots";
     }
 };
 
-class NegativeFactorial : CalculationError {
+class NegativeFactorial : public CalculationError {
+public:
     const char *what() const noexcept override {
         return "Cannot calculate faclotial of negative number";
     }
